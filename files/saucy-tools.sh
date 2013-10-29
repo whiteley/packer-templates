796b0ff4bfcbb1b3b969c679326f700ed13218f8
Matt Whiteley <mwhiteley@engineyard.com>
2013-10-29 11:31:21 -0700

    Saucy box

---
+++ files/saucy-tools.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o xtrace
+
+sudo mkdir /mnt/vmware-tools
+sudo mount -o loop linux.iso /mnt/vmware-tools
+tar zxf /mnt/vmware-tools/VMwareTools-*.tar.gz -C /tmp
+pushd /tmp/vmware-tools-distrib
+bash /tmp/vmware_hgfs_fix11.sh
+popd
+sudo /tmp/vmware-tools-distrib/vmware-install.pl --default
+rm -rf /tmp/vmware-tools-distrib
+sudo umount /mnt/vmware-tools
+sudo rmdir /mnt/vmware-tools
